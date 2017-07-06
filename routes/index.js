e89cad4bdc615fe9d9e596cf226abbc0da8645bc
seanblyth <sean@outfront.co.za>
2017-07-06 10:16:17 +0200

    added user object to put route

--- routes/index.js
+++ routes/index.js
@@ -87,6 +87,7 @@ router.put('/profile', passport.authenticate('local-signup', {
   successRedirect: '/profile',
   failureRedirect: '/profile',
   failureFlash: true,
+  user: req.user,
 }));
 
 router.get('/logout', function(req, res) {
